010588cb3067d5601970aba00301fc785d85b7fb
UsiFX <xprjkts@gmail.com>
2022-09-27 03:00:19 +0200

    magisk: functions: [1/2] refactor Patch-Contains:   ~~ refactor for following functions:   ~~   cleanup(), abort(), device_check(), cp_ch(), install_script()
    THIS PATCH IS INCOMPLETE.
    
    Signed-off-by: UsiFX <xprjkts@gmail.com>

--- magisk/common/functions.sh
+++ magisk/common/functions.sh
@@ -1,91 +1,115 @@
-abort() {
-  ui_print "$1"
-  rm -rf $MODPATH 2>/dev/null
-  cleanup
-  rm -rf $TMPDIR 2>/dev/null
-  exit 1
-}
+#!/system/bin/sh
+#
+# Utility Functions, simple MMT
+# Written by UsiFX, pedrozzz0
+#
+# Copyright Identiter: GPL-3.0
+# Copyright (C) 2022~2023 UsiFX <xprjkts@gmail.com>
+#
+
 cleanup() {
-  rm -rf $MODPATH/common 2>/dev/null
+	rm -rf "$MODPATH/common" 2>/dev/null
+	rm -rf "$MODPATH/LICENSE" 2>/dev/null
+	rm -rf "$MODPATH/README.md" 2>/dev/null
 }
+
+abort() {
+	ui_print "$1"
+ 	rm -rf $MODPATH 2>/dev/null
+ 	rm -rf $TMPDIR 2>/dev/null
+	exit 1
+}
+
 device_check() {
-  local opt=`getopt -o dm -- "$@"` type=device
-  eval set -- "$opt"
-  while true; do
-    case "$1" in
-      -d) local type=device; shift;;
-      -m) local type=manufacturer; shift;;
-      --) shift; break;;
-      *) abort "Invalid device_check argument $1! Aborting!";;
-    esac
-  done
-  local prop=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-  for i in /system /vendor /odm /product; do
-    if [ -f $i/build.prop ]; then
-      for j in "ro.product.$type" "ro.build.$type" "ro.product.vendor.$type" "ro.vendor.product.$type"; do
-        [ "$(sed -n "s/^$j=//p" $i/build.prop 2>/dev/null | head -n 1 | tr '[:upper:]' '[:lower:]')" == "$prop" ] && return 0
-      done
-      [ "$type" == "device" ] && [ "$(sed -n "s/^"ro.build.product"=//p" $i/build.prop 2>/dev/null | head -n 1 | tr '[:upper:]' '[:lower:]')" == "$prop" ] && return 0
-    fi
-  done
-  return 1
+	local opt=`getopt -o dm -- "$@"` type=device
+	eval set -- "$opt"
+	while true; do
+		case "$1" in
+			-d) local type=device; shift;;
+			-m) local type=manufacturer; shift;;
+			--) shift; break;;
+			*) abort "Invalid device_check argument $1! Aborting!";;
+		esac
+	done
+	local prop=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+	for i in /system /vendor /odm /product; do
+		[[ -f $i/build.prop ]] &&
+			for j in "ro.product.$type" "ro.build.$type" "ro.product.vendor.$type" "ro.vendor.product.$type"; do
+				[ "$(sed -n "s/^$j=//p" $i/build.prop 2>/dev/null | head -n 1 | tr '[:upper:]' '[:lower:]')" == "$prop" ] && return 0
+			done
+			[ "$type" == "device" ] && [ "$(sed -n "s/^"ro.build.product"=//p" $i/build.prop 2>/dev/null | head -n 1 | tr '[:upper:]' '[:lower:]')" == "$prop" ] && return 0
+		}
+	done
+	return $?
 }
 cp_ch() {
-  local opt=`getopt -o nr -- "$@"` BAK=true UBAK=true FOL=false
-  eval set -- "$opt"
-  while true; do
-    case "$1" in
-      -n) UBAK=false; shift;;
-      -r) FOL=true; shift;;
-      --) shift; break;;
-      *) abort "Invalid cp_ch argument $1! Aborting!";;
-    esac
-  done
-  local SRC="$1" DEST="$2" OFILES="$1"
-  $FOL && local OFILES=$(find $SRC -type f 2>/dev/null)
-  [ -z $3 ] && PERM=0644 || PERM=$3
-  case "$DEST" in
-    $TMPDIR/*|$MODULEROOT/*|$NVBASE/modules/$MODID/*) BAK=false;;
-  esac
-  for OFILE in ${OFILES}; do
-    if $FOL; then
-      if [ "$(basename $SRC)" == "$(basename $DEST)" ]; then
-        local FILE=$(echo $OFILE | sed "s|$SRC|$DEST|")
-      else
-        local FILE=$(echo $OFILE | sed "s|$SRC|$DEST/$(basename $SRC)|")
-      fi
-    else
-      [ -d "$DEST" ] && local FILE="$DEST/$(basename $SRC)" || local FILE="$DEST"
-    fi
-    if $BAK && $UBAK; then
-      [ ! "$(grep "$FILE$" $INFO 2>/dev/null)" ] && echo "$FILE" >> $INFO
-      [ -f "$FILE" -a ! -f "$FILE~" ] && { mv -f $FILE $FILE~; echo "$FILE~" >> $INFO; }
-    elif $BAK; then
-      [ ! "$(grep "$FILE$" $INFO 2>/dev/null)" ] && echo "$FILE" >> $INFO
-    fi
-    install -D -m $PERM "$OFILE" "$FILE"
-  done
+	opt=$(getopt -o nr -- "$@") BAK=true UBAK=true FOL=false
+	eval set -- "$opt"
+	while true; do
+		case "$1" in
+			-n)
+				UBAK=false
+				shift
+			;;
+			-r)
+				FOL=true
+				shift
+			;;
+			--)
+				shift
+				break
+			;;
+			*) abort "Invalid cp_ch argument $1! Aborting!" ;;
+		esac
+	done
+	SRC="$1" DEST="$2" OFILES="$1"
+	"$FOL" && OFILES=$(find "$SRC" -type f 2>/dev/null)
+	[[ -z "$3" ]] && PERM=0644 || PERM="$3"
+	case "$DEST" in
+		"$TMPDIR"/* | "$MODULEROOT"/* | "$NVBASE/modules/$MODID"/*) BAK=false ;;
+	esac
+	for OFILE in "$OFILES"; do
+		"$FOL" && {
+		[[ "$(basename "$SRC")" == "$(basename "$DEST")" ]] && FILE=$(echo "$OFILE" | sed "s|$SRC|$DEST|") || FILE=$(echo "$OFILE" | sed "s|$SRC|$DEST/$(basename "$SRC")|")
+		} || [[ -d "$DEST" ]] && FILE="$DEST/$(basename "$SRC")" || FILE="$DEST"
+		"$BAK" && "$UBAK" && {
+			[[ ! "$(grep "$FILE"$ "$INFO" 2>/dev/null)" ]] && echo "$FILE" >>"$INFO"
+			[[ -f "$FILE" -a ! -f $FILE~ ]] && {
+				mv -f "$FILE" "$FILE"~
+				echo "$FILE"~ >>"$INFO"
+			} || "$BAK" && [[ ! "$(grep "$FILE"$ "$INFO" 2>/dev/null)" ]] && echo "$FILE" >>"$INFO"
+		}
+		install -D -m "$PERM" "$OFILE" "$FILE"
+	done
 }
+
 install_script() {
-  case "$1" in
-    -l) shift; local INPATH=$NVBASE/service.d;;
-    -p) shift; local INPATH=$NVBASE/post-fs-data.d;;
-    *) local INPATH=$NVBASE/service.d;;
-  esac
-  [ "$(grep "#!/system/bin/sh" $1)" ] || sed -i "1i #!/system/bin/sh" $1
-  local i; for i in "MODPATH" "LIBDIR" "MODID" "INFO" "MODDIR"; do
-    case $i in
-      "MODPATH") sed -i "1a $i=$NVBASE/modules/$MODID" $1;;
-      "MODDIR") sed -i "1a $i=\${0%/*}" $1;;
-      *) sed -i "1a $i=$(eval echo \$$i)" $1;;
-    esac
-  done
-  [ "$1" == "$MODPATH/uninstall.sh" ] && return 0
-  case $(basename $1) in
-    post-fs-data.sh|service.sh) ;;
-    *) cp_ch -n $1 $INPATH/$(basename $1) 0755;;
-  esac
+	case "$1" in
+		-l)
+			shift
+			INPATH="$NVBASE/service.d"
+		;;
+		-p)
+			shift
+			INPATH="$NVBASE/post-fs-data.d"
+		;;
+		*) INPATH="$NVBASE/service.d" ;;
+	esac
+	[[ "$(grep "#!/system/bin/sh" "$1")" ]] || sed -i "1i #!/system/bin/sh" "$1"
+	for i in "MODPATH" "LIBDIR" "MODID" "INFO" "MODDIR"; do
+		case "$i" in
+			"MODPATH") sed -i "1a $i=$NVBASE/modules/$MODID" "$1" ;;
+			"MODDIR") sed -i "1a $i=\${0%/*}" "$1" ;;
+			*) sed -i "1a $i=$(eval echo \$$i)" "$1" ;;
+		esac
+	done
+	[[ "$1" == "$MODPATH/uninstall.sh" ]] && return 0
+	case $(basename "$1") in
+		post-fs-data.sh | service.sh) ;;
+		*) cp_ch -n "$1" "$INPATH"/"$(basename "$1")" 0755 ;;
+	esac
 }
+
 prop_process() {
   sed -i -e "/^#/d" -e "/^ *$/d" $1
   [ -f $MODPATH/system.prop ] || mktouch $MODPATH/system.prop
