6a54e06d519825754ed84245566848703e556432
UsiFX <xprjkts@gmail.com>
2022-09-30 04:12:26 +0200

    magisk: functions: load the setup script
    Signed-off-by: UsiFX <xprjkts@gmail.com>

--- magisk/common/functions.sh
+++ magisk/common/functions.sh
@@ -180,7 +180,7 @@ if [ -f "$INFO" ]; then
   rm -f "$INFO"
 fi
   "- Installing"
-[ -f "$MODPATH/common/install.sh" ] && . "$MODPATH"/common/install.sh
+[ -f "$MODPATH/setup.sh" ] && . "$MODPATH"/setup.sh
   "Installing for $ARCH SDK $API device..."
 for i in $(find "$MODPATH" -type f -name "*.sh" -o -name "*.prop" -o -name "*.rule"); do
   [ -f $i ] && { sed -i -e "/^#/d" -e "/^ *$/d" "$i"; [ "$(tail -1 "$i")" ] && echo "" >> "$i"; } || continue
