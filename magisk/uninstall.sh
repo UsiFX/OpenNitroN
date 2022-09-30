4a906b1d4ef10fbc7a69eb7932415bc5212a4cd8
UsiFX <xprjkts@gmail.com>
2022-09-30 04:22:40 +0200

    magisk: uninstall: adapt under shellcheck requirements
    Signed-off-by: UsiFX <xprjkts@gmail.com>

--- magisk/uninstall.sh
+++ magisk/uninstall.sh
@@ -1,6 +1,17 @@
+#
+# Uninstaller, simple MMT
+# Written by UsiFX, pedrozzz0
+#
+# Copyright Identiter: GPL-3.0
+# Copyright (C) 2022~2023 UsiFX <xprjkts@gmail.com>
+#
+# shellcheck disable=SC2148
+# shellcheck disable=SC2015
+#
+
 # Don't modify anything after this
 [[ -f "$INFO" ]] && {
-	while read LINE; do
+	while read -r LINE; do
 		if [[ "$(echo -n "$LINE" | tail -c 1)" == "~" ]]; then
 			continue
 		elif [[ -f "$LINE~" ]]; then
@@ -8,10 +19,10 @@
 		else
 			rm -f "$LINE"
 			while true; do
-				LINE=$(dirname $LINE)
-				[[ "$(ls -A $LINE 2>/dev/null)" ]] && break 1 || rm -rf "$LINE"
+				LINE=$(dirname "$LINE")
+				[[ "$(ls -A "$LINE" 2>/dev/null)" ]] && break 1 || rm -rf "$LINE"
 			done
 		fi
-	done < $INFO
+	done < "$INFO"
 	rm -f "$INFO"
 }
