13e1303ee7409613de07d48f6de22358db48693e
Pedrozzz0 <guitopzika26@gmail.com>
2022-10-17 00:57:55 +0200

    nitron_headers: add more detail-full information to the API
    Signed-off-by: UsiFX <xprjkts@gmail.com>

--- nitron_headers.sh
+++ nitron_headers.sh
@@ -51,6 +51,70 @@ modelockn()
 }
 
 apin() {
+	cpu_gov=$(cat "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor")
+
+	# Number of CPU cores
+	nr_cores=$(cat /sys/devices/system/cpu/possible | awk -F "-" '{print $2}')
+	nr_cores=$((nr_cores + 1))
+
+	# Battery info
+	# Current battery capacity available
+	[[ -e "/sys/class/power_supply/battery/capacity" ]] && batt_pctg=$(cat /sys/class/power_supply/battery/capacity) || batt_pctg=$(dumpsys battery 2>/dev/null | awk '/level/{print $2}')
+
+	# Battery health
+	batt_hth=$(dumpsys battery | awk '/health/{print $2}')
+	[[ -e "/sys/class/power_supply/battery/health" ]] && batt_hth=$(cat /sys/class/power_supply/battery/health)
+	case "$batt_hth" in
+		1) batt_hth="Unknown" ;;
+		2) batt_hth="Good" ;;
+		3) batt_hth="Overheat" ;;
+		4) batt_hth="Dead" ;;
+		5) batt_hth="OV" ;;
+		6) batt_hth="UF" ;;
+		7) batt_hth="Cold" ;;
+		*) batt_hth="$batt_hth" ;;
+	esac
+
+	# Battery status
+	batt_sts=$(dumpsys battery | awk '/status/{print $2}')
+	[[ -e "/sys/class/power_supply/battery/status" ]] && batt_sts=$(cat /sys/class/power_supply/battery/status)
+	case "$batt_sts" in
+		1) batt_sts="Unknown" ;;
+		2) batt_sts="Charging" ;;
+		3) batt_sts="Discharging" ;;
+		4) batt_sts="Not charging" ;;
+		5) batt_sts="Full" ;;
+		*) batt_sts="$batt_sts" ;;
+	esac
+
+	# Battery total capacity
+	batt_cpct=$(cat /sys/class/power_supply/battery/charge_full_design)
+	[[ "$batt_cpct" == "" ]] && batt_cpct=$(dumpsys batterystats | awk '/Capacity:/{print $2}' | cut -d "," -f 1)
+
+
+	androiddevinfo()
+	{
+		# Device info
+		# Codename
+		dvc_cdn=$(getprop ro.product.device)
+
+		# Device brand
+		dvc_brnd=$(getprop ro.product.brand)
+
+		# ROM info
+		# Fingerprint, keys and related stuff
+		rom_info=$(getprop ro.build.description | awk '{print $1,$3,$4,$5}')
+		[[ "$rom_info" == "" ]] && rom_info=$(getprop ro.bootimage.build.description | awk '{print $1,$3,$4,$5}')
+		[[ "$rom_info" == "" ]] && rom_info=$(getprop ro.system.build.description | awk '{print $1,$3,$4,$5}')
+
+		# ARV (Android release version)
+		arv=$(getprop ro.build.version.release)
+
+		echo "Android Device Codename: $dvc_cdn"
+		echo "Android Device Brand: $dvc_brnd"
+		echo "Android ROM Info: $rom_info"
+		echo "Android Release Version: $arv"
+	}
 	resrchk()
 	{
 		echo "PID: $$"
@@ -59,6 +123,13 @@ apin() {
 		echo "Memory: $(( $(infogrbn "/proc/meminfo" "MemTotal") / 1024 / 1024))gb"
 		echo "Hardware: $(infogrblongn "/proc/cpuinfo" "Hardware")"
 		echo "Machine: $(uname -m)"
+		echo "CPU Governor: $cpu_gov"
+		echo "CPU Cores: $nr_cores"
+		echo "Battery Percentage: $batt_pctg%"
+		echo "Battery Health: $batt_hth"
+		echo "Battery Status: $batt_sts"
+		echo "Battery Capacity: $batt_cpct"
+		[[ "$PLATFORM" == "Android" ]] && androiddevinfo
 	}
 
 	__api_help()
