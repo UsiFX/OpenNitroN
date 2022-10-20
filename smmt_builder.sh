2ebbc4a44cccf202b5311880c33a4e6adaa332f4
UsiF <xprjkts@gmail.com>
2022-10-20 03:43:16 +0200

    treewide: bump `1.0.6-staging`
    Signed-off-by: UsiF <xprjkts@gmail.com>

--- smmt_builder.sh
+++ smmt_builder.sh
@@ -13,7 +13,7 @@
 #
 
 TIMESTAMP=$(date +%Y%m%d)
-VERSION='1.0.5-staging'
+VERSION='1.0.6-staging'
 OBJECTS=("nitrond" "nitron_headers.sh")
 MMT_OBJECTS=("magisk/META-INF/com/google/android/update-binary" "magisk/setup.sh" "magisk/common/functions.sh" "magisk/uninstall.sh")
 PLACEHOLDERS=("debian/usr/placeholder" "debian/usr/bin/placeholder" "debian/usr/include/placeholder")
