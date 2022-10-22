0429bbf1cd397fd8f22b54c44b5b68ab5b0c9ca2
UsiF <xprjkts@gmail.com>
2022-10-22 10:07:32 +0200

    libauto: change logic & run only under the configured pkgs
    Signed-off-by: UsiF <xprjkts@gmail.com>

--- libauto.sh
+++ libauto.sh
@@ -14,39 +14,34 @@ com.activision.callofduty.shooter
 " >> "$NITRON_RELAX_DIR/nitron.auto.conf"
 
 pkgs=$(cat "$NITRON_RELAX_DIR/nitron.auto.conf")
-pkggrab=$(ps -A -o %CPU,NAME)
-pidgrab=$(ps -A -o PID)
+printavailpids=$(ps -A -o PID)
 cpuisinload=$(ps -A -o %CPU | awk -F: '{if($1>75)print$1}')
 cpuisinsuffer=$(ps -A -o %CPU | awk -F: '{if($1>85)print$1}')
+
+printn -ll "nitrond: starting modifying modes up on Packages availability & Resource usage"
 
 auto()
 {
 	apin -cl
-	if [[ "$batt_sts" == "Charging" ]]; then
-		magicn -g
-		printn -ll "system is charging, relaxing with green mode"
-	fi
-	if [[ "batt_pctg" -lt "25" ]]; then
-		magicn -g
-		printn -ll "battery is under %25, applied green mode"
-	fi
-	if [[ "$cpuisinsuffer" -gt 75 ]]; then
-		magicn -r
-		printn -ll "cpu is under load applied Red mode, consuming battery."
-	elif [[ "$cpuisinload" -gt 85 ]]; then
-		magicn -y
-		printn -ll "heavy process(es) detected, applied balance mode"
-	fi
 	for relax in $(pidof ${pkgs[@]})
 	do
-		if [[ "$pidgrab" == *"$relax"* ]] ; then
-			magicn -r
-			printn -ll "detected running app which available in configs, applied Red mode."
+		if [[ "$relax" == *"$printavailpids"* ]]; then
+			if [[ "batt_pctg" -lt "25" ]]; then
+				magicn -g
+				printn -ll "battery is under %25, applied green mode"
+			fi
+			if [[ "$cpuisinsuffer" -gt 75 ]]; then
+				magicn -y
+				printn -ll "heavy process(es) detected, applied balance mode"
+			elif [[ "$cpuisinload" -gt 85 ]]; then
+				magicn -r
+				printn -ll "cpu is under load applied Red mode, consuming battery."
+			fi
 		fi
 	done
 }
 
 while true; do
-	sleep 100
+	sleep 120
 	auto
 done
