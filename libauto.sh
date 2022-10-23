81c28cd65ec6938bc6a7ad7e8e300feba5d3d2d2
UsiF <xprjkts@gmail.com>
2022-10-23 15:10:37 +0200

    libauto: change mode application algorithm
    Patch-Provides:
      ~~ refer to cpu total usage, not proccess(es) one's
      ~~ apply the mode for one time, do not if it was previously
    
    Signed-off-by: UsiF <xprjkts@gmail.com>

--- libauto.sh
+++ libauto.sh
@@ -1,4 +1,8 @@
 #
+# shellcheck disable=SC2148
+# shellcheck disable=SC2034
+# shellcheck disable=SC2154
+#
 # nitronD AutoMaticProfiler
 #
 # Copyright Identiter: GPL-3.0
@@ -14,26 +18,31 @@ com.activision.callofduty.shooter
 " >> "$NITRON_RELAX_DIR/nitron.auto.conf"
 
 NITRON_LIBAUTO_VERSION='1.0.2'
+SOURCE="libauto"
+MODE=$(apin -mc)
 pkgs=$(cat "$NITRON_RELAX_DIR/nitron.auto.conf")
-cpuisinload=$(ps -A -o %CPU | awk -F: '{if($1>75)print$1}' | cut -f1 -d\.)
-cpuisinsuffer=$(ps -A -o %CPU | awk -F: '{if($1>85)print$1}' | cut -f1 -d\.)
+cpuisinload=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage ""}' | awk -F: '{if($1>85)print$1}' | cut -f1 -d\.)
+cpuisinsuffer=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage ""}' | awk -F: '{if($1>75)print$1}' | cut -f1 -d\.)
+cputotalusage=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage ""}' | cut -f1 -d\.)
 
 printn -ll "nitrond: starting modifying modes up on Packages availability & Resource usage"
 
 auto()
 {
 	apin -cl
-	for relax in "$(pidof ${pkgs[@]} | tr ' ' '\n')"
+	for relax in $(pidof "${pkgs[@]}" | tr ' ' '\n')
 	do
-		if [[ "$(ps -A -o PID | grep "$relax")" ]]; then
-			if [[ "batt_pctg" -lt "25" ]]; then
-				magicn -g
+		printn -ll "started"
+		if ps -A -o PID | grep -q "$relax"; then
+			if [[ "$batt_pctg" -lt "25" ]]; then
+				[[ ${MODE[2]} != "[green]" ]] && magicn -g
 				printn -ll "battery is under %25, applied green mode"
 			fi
 			if [[ "$cpuisinsuffer" -gt 75 ]]; then
-				magicn -y
+				[[ ${MODE[2]} != "[yellow]" ]] && magicn -y
 				printn -ll "heavy process(es) detected, applied balance mode"
 			elif [[ "$cpuisinload" -gt 85 ]]; then
+				[[ ${MODE[2]} != "[red]" ]] && magicn -r
 				magicn -r
 				printn -ll "cpu is under load applied Red mode, consuming battery."
 			fi
