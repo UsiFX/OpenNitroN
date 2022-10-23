d92160122dd7e497d4d2c383e3f8f26313c4630d
UsiF <xprjkts@gmail.com>
2022-10-23 22:36:09 +0200

    libauto: fixup! refactor mechanism
    Signed-off-by: UsiF <xprjkts@gmail.com>

--- libauto.sh
+++ libauto.sh
@@ -18,40 +18,38 @@ com.activision.callofduty.shooter
 " >> "$NITRON_RELAX_DIR/nitron.auto.conf"
 
 NITRON_LIBAUTO_VERSION='1.0.2'
-SOURCE="libauto"
-MODE=$(apin -mc)
 pkgs=$(cat "$NITRON_RELAX_DIR/nitron.auto.conf")
-cpuisinload=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage ""}' | awk -F: '{if($1>85)print$1}' | cut -f1 -d\.)
-cpuisinsuffer=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage ""}' | awk -F: '{if($1>75)print$1}' | cut -f1 -d\.)
-cputotalusage=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage ""}' | cut -f1 -d\.)
-
+relax=$(pidof "${pkgs[@]}" | tr ' ' '\n')
 auto()
 {
-	for relax in $(pidof "${pkgs[@]}" | tr ' ' '\n')
-	do
-		printn -ll "started"
-		if ps -A -o PID | grep -q "$relax"; then
-			if [[ "$batt_pctg" -lt "25" ]]; then
-				[[ ${MODE[2]} != "[green]" ]] && magicn -g
-				printn -ll "battery is under %25, applied green mode"
-			fi
-			if [[ "$cpuisinsuffer" -gt 75 ]]; then
-				[[ ${MODE[2]} != "[yellow]" ]] && magicn -y
-				printn -ll "heavy process(es) detected, applied balance mode"
-			elif [[ "$cpuisinload" -gt 85 ]]; then
-				[[ ${MODE[2]} != "[red]" ]] && magicn -r
-				magicn -r
-				printn -ll "cpu is under load applied Red mode, consuming battery."
-			fi
+		SOURCE="libauto"
+		if [[ "$(ps -A -o PID | grep "$relax")" ]]; then
+			[[ "$batt_pctg" -lt "25" ]] && {
+				if [[ "$(apin -mc | awk '{print $2}')" != "green" ]]; then
+					magicn -g
+					printn -ll "battery is under %25, applied green mode"
+				fi
+			} || {
+				if [[ "$cputotalusage" -gt "70" ]]; then
+					[[ "$(apin -mc | awk '{print $2}')" != "yellow" ]] || {
+						magicn -y
+						printn -ll "heavy process(es) detected, applied balance mode."
+					}
+				elif [[ "$cputotalusage" -gt "85" ]]; then
+					[[ "$(apin -mc | awk '{print $2}')" != "red" ]] || {
+						magicn -r
+						printn -ll "cpu is under load applied Red mode, consuming battery."
+					}
+				fi
+			}
 		fi
-	done
 }
 
 case $@ in
 	"-d" | "--daemon")
 		while true; do
-			sleep 120
 			auto
+			sleep 60
 		done
 	;;
 esac
