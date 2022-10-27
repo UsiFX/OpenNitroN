74ffa79e8a8a2bc097d1d7a2cb50699c340a4b7b
UsiF <xprjkts@gmail.com>
2022-10-27 02:33:22 +0200

    smmt_builder: update main obj(s)
    Signed-off-by: UsiF <xprjkts@gmail.com>

--- smmt_builder.sh
+++ smmt_builder.sh
@@ -14,7 +14,7 @@
 
 TIMESTAMP=$(date +%Y%m%d)
 VERSION='1.0.8-staging'
-OBJECTS=("nitrond" "nitron_headers.sh" "libauto.sh")
+OBJECTS=("nitrond" "nitronapi.sh")
 MMT_OBJECTS=("magisk/META-INF/com/google/android/update-binary" "magisk/setup.sh" "magisk/common/functions.sh" "magisk/uninstall.sh" "magisk/system.prop")
 PLACEHOLDERS=("debian/usr/placeholder" "debian/usr/bin/placeholder" "debian/usr/include/placeholder")
 FILENAME="NitronX-$VERSION-$RANDOM-$TIMESTAMP"
