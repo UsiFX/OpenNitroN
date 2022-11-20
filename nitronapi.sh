c6cf64b80ef8ef027d0df2e91bfafc4c9a31a822
UsiFX <xprjkts@gmail.com>
2022-11-20 23:17:56 +0200

    nitronapi: hide mode change outputs
    Signed-off-by: UsiFX <xprjkts@gmail.com>

--- nitronapi.sh
+++ nitronapi.sh
@@ -335,20 +335,20 @@ com.activision.callofduty.shooter
 				autoalg() {
 						if [[ "$batt_pctg" -lt "25" ]]; then
 							if [[ "$(apin -mc | awk '{print $2}')" != "green" ]]; then
-								magicn -g
+								magicn -g 2>&1 >/dev/null
 								printn -ll "battery is under %25, applied green mode"
 							fi
 						else
 							if (( cputotalusage >= "50" && cputotalusage <= "64" )); then
 								if [[ "$(apin -mc | awk '{print $2}')" != "yellow" ]]; then
 									printn -ll "cpu usage is 50%+"
-									magicn -y
+									magicn -y 2>&1 >/dev/null
 									printn -ll "heavy process(es) detected, applied balance mode."
 								fi
 							elif (( cputotalusage >= "65" )); then
 								if [[ "$(apin -mc | awk '{print $2}')" != "red" ]]; then
 										printn -ll "cpu usage is 65%+"
-										magicn -r
+										magicn -r 2>&1 >/dev/null
 										printn -ll "cpu is under load applied Red mode, consuming battery."
 								fi
 							fi
